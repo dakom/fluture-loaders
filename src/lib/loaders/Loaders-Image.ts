0a9e7e7d4e8cb28351d91959d2e84917f5e0d2fa
dakom <david.komer@gmail.com>
2018-03-14 23:48:46 +0200

    export loadImageUrlNoCrossOrigin

--- src/lib/loaders/Loaders-Image.ts
+++ src/lib/loaders/Loaders-Image.ts
@@ -2,6 +2,9 @@ import {S, Maybe, Either} from "../external/sanctuary/Sanctuary";
 import {sameOrigin, isNil} from "../utils/Utils";
 import {Future} from "fluture";
 
+//The typical use case for loading images this way would be for WebGL Texture generation and things like that
+//Therefore the default loadImageUrl is the simplest case of auto-detecting the need for crossOrigin setting
+
 export const loadImageUrlCrossOrigin = (crossOrigin: Maybe<"anonymous" | "use-credentials">) => (url:string): Future<ErrorEvent, HTMLImageElement> => Future((reject, resolve) => {
     const img = new Image();
     
@@ -13,6 +16,8 @@ export const loadImageUrlCrossOrigin = (crossOrigin: Maybe<"anonymous" | "use-cr
 
     img.src = url;
 });
+
+export const loadImageUrlNoCrossOrigin = loadImageUrlCrossOrigin(S.Nothing);
 
 export const loadImageUrl = (url:string): Future<Event, HTMLImageElement> =>
     loadImageUrlCrossOrigin(!sameOrigin(url) ? S.Just("anonymous") : S.Nothing) (url);
