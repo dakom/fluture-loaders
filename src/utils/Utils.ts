deb6c5ccfd8f58e505fe0da7f13d0012ba69df9e
David Komer <david.komer@gmail.com>
2018-03-21 10:39:29 +0200

    same origin

--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -2,4 +2,7 @@ export const isNil = (x:any):boolean => x == undefined || x == null;
 
 export const DOMURL = window.URL || (window as any).webkitURL || window;
 
-export const sameOrigin = (url:string):boolean => (new URL(url).origin === window.location.origin);
+export const sameOrigin = (url:string):boolean => 
+    url.indexOf("http://") !== -1 || url.indexOf("https://") !== -1
+        ?   (new URL(url).origin === window.location.origin)
+        :   true;
