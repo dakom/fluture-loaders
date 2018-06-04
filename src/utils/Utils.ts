1854fc28d5be62b0b31fb5f036c94e67631a543f
David Komer <david.komer@gmail.com>
2018-06-05 00:54:51 +0300

    window url

--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -1,8 +1,11 @@
 export const isNil = (x:any):boolean => x == undefined || x == null;
 
-export const DOMURL = window.URL || (window as any).webkitURL || window;
+export const getDomUrl = (url:string) => {
+    const ctor = window.URL || (window as any).webkitURL || window;
+    return new ctor(url);
+}
 
 export const sameOrigin = (url:string):boolean => 
     url.indexOf("http://") !== -1 || url.indexOf("https://") !== -1
-        ?   (new URL(url).origin === window.location.origin)
+        ?   (getDomUrl(url).origin === window.location.origin)
         :   true;
